62a4317246e4fe14529d47539266632d49b154bc
dontdisturbiamcoding <dontdisturbiamcoding@gmail.com>
2022-11-03 06:40:07 +0100

    Added a function to personalize the greeting based on the time

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Variable de nombre de usuario
+const userName = "Jordi";
+
 // Conseguir un número aleatorio para cambiar el fondo
 let numAleatorio = Math.floor(Math.random()*3)+1;
 
 
-
+// Cambiar el fondo según el numero Aleatorio
 function cambioFondo (numAleatorio) {
     let background;
     switch (numAleatorio) {
@@ -20,6 +23,7 @@ function cambioFondo (numAleatorio) {
     document.querySelector(".container").style.backgroundImage = `url(${background})` ;
 }
 
+// Mostrar hora en pantalla
 function horaActual () {
     let getTime = new Date();
     let hours = getTime.getHours();
@@ -34,11 +38,24 @@ function horaActual () {
         minutes = "0" + minutes;
     } 
     document.getElementById("hora").innerHTML = `${hours}:${minutes}:${seconds}`;
-
+    saludo(hours); // Llamamos a la función que genera el saludo en función de hours
 }
+
+// Cambiar el saludo en función de la hora
+function saludo (hours) {
+    if (hours < 12) {
+        document.querySelector(".saludo").textContent = "Buenos días " + userName;
+    } else if (hours < 21) {
+        document.querySelector(".saludo").textContent = "Buenas tardes " + userName;
+    } else {
+        document.querySelector(".saludo").textContent = "Buenas noches " + userName;
+    }
+}
+
 
 cambioFondo(numAleatorio);
 horaActual();
+
 function actualizarHora () {
     setInterval(horaActual,1000)
 };
